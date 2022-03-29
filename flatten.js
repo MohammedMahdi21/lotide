265e1ac4d12871fe1f99993e5dca6f0c1c2c5739
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-03-29 01:52:30 +0000

    Array flatten function

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`👍🏼👍🏼👍🏼 PASSED , Array 1 = Array 2`);
+  } else {
+    console.log(`⛔️⛔️⛔️ FAILED , Array 1 != Array 2`);
+  }
+};
+
+
+const eqArrays = function(actual, expected) {
+  let j = 0;
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] === expected[j]) {
+      j++;
+    } else {
+      return false;
+    }
+
+  } return true;
+
+};
+
+const flatten = function(arrayToFlatten) {
+  const flattened = [];
+  for (let item of arrayToFlatten) {
+    if (Array.isArray(item)) {
+      for (let nested of item) {
+        flattened.push(nested);
+      }
+    } else {
+      flattened.push(item);
+    }
+  }
+  console.log(flattened);
+};
+
+
