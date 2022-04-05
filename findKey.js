a8d2eca0d6943a133fb3391c1e1f8876744a05c0
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-05 05:07:22 +0000

    function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (expected === actual) {
+    console.log(`👍🏼👍🏼👍🏼 ${actual} === expected`);
+  } else {
+    console.log(`⛔️⛔️⛔️ ${actual} !== expected`);
+  }
+};
+
+
+const findKey = function(object, callBack) {
+
+  for (let keys in object) {
+
+    if (callBack(object[keys])) {
+      return keys;
+    }
+  }
+  return undefined;
+};
+
+
+
+
+//const result = findKey({
+//  "Blue Hill": { stars: 1 },
+//  "Akaleri":   { stars: 3 },
+//  "noma":      { stars: 2 },
+//  "elBulli":   { stars: 3 },
+//  "Ora":       { stars: 2 },
+//  "Akelarre":  { stars: 3 }
+//}, x => x.stars === 2);  // => "noma"
+//
+//assertEqual(result, "noma");
