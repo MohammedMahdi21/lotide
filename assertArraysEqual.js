9b023e8340de1b4690a442d631ef90b2ebf4df98
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-15 13:09:00 +0000

    refactor the function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,13 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
-    console.log(`👍🏼👍🏼👍🏼 PASSED , Array 1 = Array 2`);
+    console.log(`👍🏼👍🏼👍🏼 the actual: ${actual} === the expected: ${expected}`);
   } else {
-    console.log(`⛔️⛔️⛔️ FAILED , Array 1 != Array 2`);
+    console.log(`⛔️⛔️⛔️ the actual: ${actual} !== the expected: ${expected}`);
   }
 };
 
-
-const eqArrays = function(actual, expected) {
-
-  if (actual.length !== expected.length) {
-
-    return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-
-    }
-  }
-  return true;
-
-};
+module.exports = assertArraysEqual;
 
 
