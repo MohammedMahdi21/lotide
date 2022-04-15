c3921abd66003cf222a5f65375b5a45d2c2c67da
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-15 13:39:30 +0000

    refactor the function

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`👍🏼👍🏼👍🏼 PASSED , Array 1 = Array 2`);
-  } else {
-    console.log(`⛔️⛔️⛔️ FAILED , Array 1 != Array 2`);
-  }
-};
-
-
-const eqArrays = function(actual, expected) {
-  let j = 0;
-
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] === expected[j]) {
-      j++;
-    } else {
-      return false;
-    }
-
-  } return true;
-
-};
-
 const middle = function(array) {
   const middleElement = [];
   let counter = 0;
@@ -39,5 +16,8 @@ const middle = function(array) {
 
   return middleElement;
 };
+
+
+module.exports = middle;
 
 
