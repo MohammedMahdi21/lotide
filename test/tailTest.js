c5b0565c8feedf9a016724af0fe9e9f9930ec9b1
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-16 04:32:29 +0000

    using mocha framework and chai assertion library

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,13 @@
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual');
+//const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertArraysEqual(result, ["Lighthouse", "Labs"]); // => PASS!
+describe("#tail", () => {
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
+
+//const result = tail(["Hello", "Lighthouse", "Labs"]);
+//assertArraysEqual(result, ["Lighthouse", "Labs"]); // => PASS!
