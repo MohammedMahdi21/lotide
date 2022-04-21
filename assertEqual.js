5643af0847b47e3434957c490679b362973bf384
MohammedMahdi21 <mmd.mahdi21@gmail.com>
2022-04-21 19:14:14 +0000

    remove gitignore files

--- assertEqual.js
+++
@@ -1,9 +0,0 @@
-const assertEqual = function(actual, expected) {
-  if (expected === actual) {
-    console.log(`👍🏼👍🏼👍🏼 the actual: ${actual} === the expected: ${expected}`);
-  } else {
-    console.log(`⛔️⛔️⛔️ the actual: ${actual} !== the expected: ${expected}`);
-  }
-};
-
-module.exports = assertEqual;
--- tail.js
+++
@@ -1,7 +0,0 @@
-const tail = function(array) {
-  let arrayTail = [];
-  arrayTail = array.slice(1);
-  return arrayTail;
-};
-
-module.exports = tail;
